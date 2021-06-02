e32fe01748289a52ea522f55f399274631b51b9e
ivVane <iv.vane@gmail.com>
2021-06-03 00:17:20 +0200

    Email fragment and layout configuration

--- app/src/main/java/com/vane/android/replycl/ui/email/EmailFragment.kt
+++ app/src/main/java/com/vane/android/replycl/ui/email/EmailFragment.kt
@@ -1,7 +1,76 @@
 package com.vane.android.replycl.ui.email
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.GridLayoutManager
 import com.vane.android.replycl.R
+import com.vane.android.replycl.data.EmailStore
+import com.vane.android.replycl.databinding.FragmentEmailBinding
+import kotlin.LazyThreadSafetyMode.*
 
-class EmailFragment : Fragment(R.layout.fragment_email) {
+private const val MAX_GRID_SPANS = 3
+
+/**
+ * A [Fragment] which displays a single, full email.
+ */
+class EmailFragment : Fragment() {
+
+    private val args: EmailFragmentArgs by navArgs()
+    private val emailId: Long by lazy(NONE) { args.emailId }
+
+    private lateinit var binding: FragmentEmailBinding
+    private val attachmentAdapter = EmailAttachmentGridAdapter(MAX_GRID_SPANS)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Set up MaterialContainerTransform transition as sharedElementEnterTransition.
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentEmailBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.navigationIcon.setOnClickListener {
+            findNavController().navigateUp()
+        }
+
+        val email = EmailStore.get(emailId)
+        if (email == null) {
+            showError()
+            return
+        }
+
+        binding.run {
+            this.email = email
+
+            // Set up the staggered/masonry grid recycler
+            attachmentRecyclerView.layoutManager = GridLayoutManager(
+                requireContext(),
+                MAX_GRID_SPANS
+            ).apply {
+                spanSizeLookup = attachmentAdapter.variableSpanSizeLookup
+            }
+            attachmentRecyclerView.adapter = attachmentAdapter
+            attachmentAdapter.submitList(email.attachments)
+        }
+    }
+
+    private fun showError() {
+        // Do nothing
+    }
 }
